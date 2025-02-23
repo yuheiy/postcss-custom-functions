6bba6fd7b876e2e644e139616449d1bcd2e05fc5
Yuhei Yasuda <yuhei.yasuda1003@gmail.com>
2025-02-24 00:49:29 +0900

    refactor tests

--- test/_tape.js
+++ test/_tape.js
@@ -6,11 +6,30 @@ postcssTape(plugin)({
 		message: 'supports basic usage',
 		options: {
 			functions: {
-				'--shadow': (shadowColor) =>
-					`2px 2px ${shadowColor ? shadowColor : 'var(--shadow-color, black)'}`,
+				'--shadow': (shadowColor = 'var(--shadow-color, black)') =>
+					`2px 2px ${shadowColor}`,
 				'--negative': (value) => `calc(-1 * ${value})`,
 				'--repeat': (a, b) => `repeat(${a}, ${b})`,
 				'--comma-separated-list-of-component-values': () => '10px, 20px',
+			},
+		},
+	},
+	'syntax-errors': {
+		message: 'warn syntax errors',
+		options: {
+			functions: {
+				'--negative': (value) => `calc(-1 * ${value})`,
+			},
+		},
+		warnings: 3,
+	},
+	'function-errors': {
+		message: 'warn function errors',
+		options: {
+			functions: {
+				'--error': () => {
+					throw new Error('function error');
+				},
 			},
 		},
 		warnings: 3,
